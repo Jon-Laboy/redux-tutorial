d09834274ff5eccc2806ed61cb7c7c27ee19c2d9
Jon Laboy <jonlaboyoc@gmail.com>
2021-05-14 12:56:56 -0400

    redux counter complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import {useSelector, useDispatch} from 'react-redux';
+import { increment, decrement } from './actions';
 
 function App() {
+  
+const counter = useSelector(state => state.counter)
+const isLogged = useSelector(state => state.isLogged)
+const dispatch = useDispatch(); 
+
   return (
     <div className="App">
-     Hi
+     <h1>Counter {counter}</h1>
+
+    {isLogged? <h3>Valuable Info I shouldn't see</h3> : "" }
+     
+     <button onClick={()=> dispatch(increment(5))}>+</button>
+     <button onClick={()=> dispatch(decrement())}>-</button>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import allReducers from './reducers';
+import { createStore } from 'redux';
+import { Provider } from "react-redux";
+
+const store = createStore(allReducers);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store = {store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
